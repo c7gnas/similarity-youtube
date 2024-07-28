6f6e9fc970269ffb1f999dd8eee65d7dc6e65eeb
Aniruddha <azizul.muktadir.chowdhury@g.bracu.ac.bd>
2024-07-28 16:49:52 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+import { colors } from 'tailwindcss/colors'
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailWindcss').Config} */
+export const darkMode = ['class']
+export const content = [
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+  // Or if using `src` directory:
+  "./src/**/*.{js,ts,jsx,tsx,mdx}",
+]
+export const theme = {
+  container: {
+    center: true,
+    padding: '1.5rem',
+    screens: {
+      '2xl': '1360px',
+    }
+  },
+  extend: {
+    fontFamily: {
+      sans: ['var(--font-inter)', ...fontFamily.sans],
+    },
+    colors: {
+      ...colors,
+      'light-gold': '#f5bc51',
+      'dark-gold': '#533519',
+    }
+  },
+}
+export const plugins = [require('tailwindcss-animate'), require('@tailwindcss/typography')]
